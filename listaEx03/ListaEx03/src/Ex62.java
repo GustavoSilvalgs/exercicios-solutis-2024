b019f91beef623669371c575c6cb0428c0cfc79b
GustavoSilvalgs <luisgust06@gmail.com>
2024-08-19 22:24:12 -0300

    add exercise 62

---
+++ listaEx03/ListaEx03/src/Ex62.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex62 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de linhas da pirâmide: ");
+        int linhas = scanner.nextInt();
+
+        for (int i = 1; i <= linhas; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(String.format("%02d", i) + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+
