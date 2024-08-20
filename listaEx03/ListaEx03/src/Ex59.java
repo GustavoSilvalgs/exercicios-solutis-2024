7b192b795e4e511e42d789f028a987fad21849cc
GustavoSilvalgs <luisgust06@gmail.com>
2024-08-19 22:15:56 -0300

    add exercise 59

---
+++ listaEx03/ListaEx03/src/Ex59.java
@@ -0,0 +1,14 @@
+public class Ex59 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabuada do " + i + ":");
+
+            for (int j = 1; j <= 10; j++) {
+                int resultado = i * j;
+                System.out.println(i + " x " + j + " = " + resultado);
+            }
+
+            System.out.println();
+        }
+    }
+}
