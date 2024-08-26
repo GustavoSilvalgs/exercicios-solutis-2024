8699ae476bda0445007e4df4f64281c474a432f0
GustavoSilvalgs <luisgust06@gmail.com>
2024-08-25 23:14:51 -0300

    Adiciona Ex 03 Herança e Polimorfismo

--- herancaPolimorfismo/src/Main.java
+++ herancaPolimorfismo/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Animal animal;
+        /*Animal animal;
 
         animal = new Cachorro("Toto", 5);
         animal.emitirSom();
@@ -9,6 +9,16 @@ public class Main {
         animal.emitirSom();
 
         animal = new Preguica("Dorminhoco", 3);
-        animal.emitirSom();
+        animal.emitirSom();*/
+
+        Veterinario veterinario = new Veterinario();
+
+        Animal cachorro = new Cachorro("Toto", 5);
+        Animal cavalo = new Cavalo("Alazao", 8);
+        Animal preguica = new Preguica("Dorminhoco", 3);
+
+        veterinario.examinar(cachorro);
+        veterinario.examinar(cavalo);
+        veterinario.examinar(preguica);
     }
 }
---
+++ herancaPolimorfismo/src/Veterinario.java
@@ -0,0 +1,8 @@
+public class Veterinario {
+
+    public void examinar(Animal animal) {
+        System.out.println("Examinando o(a): " + animal.getNome());
+        animal.emitirSom();
+    }
+}
+
